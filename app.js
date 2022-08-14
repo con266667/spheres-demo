17acff52295a570ac01250631eff5e273212680a
Connor Wilson <cnwilson29@gmail.com>
2022-08-14 17:37:49 -0400

    probably changing everything

--- app.js
+++ app.js
@@ -5,48 +5,74 @@ const animationAttributes = [
   "opacity",
   "x",
   "y",
+  "backgroundImage"
 ];
 
 
-function addTimeline (element, timeline) {
+function addTimeline (element, timeline, triggerHook, offset) {
   new ScrollMagic.Scene({
       triggerElement: element,
-      triggerHook: 0,
+      triggerHook: triggerHook,
+      offset: offset,
       duration: "100%",
   })
       .setTween(timeline)
       .setPin(element)
+      .addIndicators()
       .addTo(controller);
 }
 
-function addTimelineElementAnimations (element, timeline) {
+function addAnimationsToTimeline (element, timeline, isFinal) {
   var prevDuration = 0;
-  animationAttributes.forEach(attribute => {
-    const attributeValue = element.dataset[attribute];
-    if (attributeValue) {
-      const duration = parseInt(element.dataset[`${attribute}Duration`]) || 0;
+
+  animationAttributes.forEach(attribute_raw => {
+    var attribute = attribute_raw;
+    var attributeValue = element.dataset[attribute];
+    if (isFinal) {
+      attribute = 'final' + attribute.charAt(0).toUpperCase() + attribute.slice(1);
+      attributeValue = element.dataset[attribute];
+    }
+    
+    if (attributeValue !== undefined) {
+      console.log(attribute);
+      const duration = parseInt(element.dataset[`${attribute}Duration`]) || 10;
       const delay = prevDuration - (parseInt(element.dataset[`${attribute}Delay`]) || 0);
-      timeline.to(element, duration, {[attribute]: attributeValue, ease: Power1.easeInOut, force3D: false}, delay < 0 ? `+=${delay}` : `-=${delay}`);
+      timeline.to(element, duration, {[attribute_raw]: attributeValue, ease: Power1.easeInOut, force3D: false}, delay < 0 ? `+=${delay}` : `-=${delay}`);
       prevDuration = duration + delay;
     }
-  }
-  );
+  });
 
   if (Array.from(element.children).length > 0) {
     Array.from(element.children).forEach(child => {
-      addTimelineElementAnimations(child, timeline);
+      addAnimationsToTimeline(child, timeline, isFinal);
     }
     );
   }
 }
 
+function addStartAnimations(element, timeline) {
+  Array.from(element.children).forEach(child => {
+    addAnimationsToTimeline(child, timeline, false);
+  });
+}
+
+function addFinalAnimations(element, timeline) {
+  Array.from(element.children).forEach(child => {
+    addAnimationsToTimeline(child, timeline, true);
+  });
+}
+
 document.querySelectorAll('[data-animation]')
   .forEach(element => {
-    const timeline = new TimelineLite()
+    const timeline = new TimelineLite();
     
-    Array.from(element.children).forEach(child => {
-      addTimelineElementAnimations(child, timeline);
-    });
+    addStartAnimations(element, timeline);
+    addFinalAnimations(element, timeline);
 
-    addTimeline(element, timeline);
-});
+    const triggerHook = parseFloat(element.dataset.triggerHook) || 0;
+    const offset = parseFloat(element.dataset.offset) || 0;
+    addTimeline(element, timeline, triggerHook, offset);
+  }
+);
+
+document.querySelector(".scrollmagic-pin-spacer").classList.style.paddingTop = "0px";

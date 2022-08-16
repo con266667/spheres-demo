e0296af8a4078df49224b90e1017ea55d457715e
Connor Wilson <cnwilson29@gmail.com>
2022-08-16 00:38:34 -0400

    bg :(

--- animatedBackground.js
+++ animatedBackground.js
@@ -1,55 +1,80 @@
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
 const bg = document.querySelector('.background')
 const color = Math.floor(Math.random() * 16777215).toString(16)
 bg.style.backgroundColor = '#' + color
 
-for (let i = 0; i < 6; i++) {
-    const circle = document.createElement('div')
-    circle.classList.add('circle')
+function randomPosition(width = 800, height = 800) {
+    var x,y;
 
-    // Random Position
+    if (isSafari) {
+        x = Math.random() * (window.innerWidth + 100) - 50
+        y = Math.random() * (window.innerHeight + 100) - 50
+    } else {
+        x = Math.random() * (window.innerWidth - width + 400) - (width + 200)
+        y = Math.random() * (window.innerHeight - height + 400) - (height + 200)
+    }
 
-    const x = Math.random() * (window.innerWidth + 100) - 50
-    const y = Math.random() * (window.innerHeight + 100) - 50
-    circle.style.transform = `translate(${x}px, ${y}px)`
-
-    // Random Color
-
-    const color = Math.floor(Math.random() * 16777215).toString(16)
-    // circle.style.backgroundColor = `#${color}`
-    circle.style.backgroundColor = 'transparent'
-
-    // Random Size between 400px and 700px
-
-    const width = Math.random() * (700 - 400) + 400
-    const height = Math.random() * (700 - 400) + 400
-    circle.style.width = `${0.1}px`
-    circle.style.height = `${0.1}px`
-
-    // Feather border
-
-    circle.style.boxShadow = `0 0 ${height}px ${width}px #${color}`
-    // circle.style.zIndex = '1'
-    // circle.style.position = 'absolute'
-
-    // Random movement along a bezier curve
-
-    const timeline = new TimelineMax({repeat: -1, yoyo: true})
-    timeline.to(circle, 10, {
-        bezier: {
-            type: 'soft',
-            curviness: 2,
-            values: [
-                { x: x, y: y },
-                { x: Math.random() * (window.innerWidth + 100) - 50, y: Math.random() * (window.innerHeight + 100) - 50 },
-                { x: Math.random() * (window.innerWidth + 100) - 50, y: Math.random() * (window.innerHeight + 100) - 50 },
-                { x: Math.random() * (window.innerWidth + 100) - 50, y: Math.random() * (window.innerHeight + 100) - 50 },
-            ],
-        },
-        ease: Power1.easeInOut,
-        force3D: false,
-    })
-
-
-    bg.appendChild(circle)
+    return { 
+        x:x, 
+        y:y 
+    }
 }
 
+function start() {
+    for (let i = 0; i < 5; i++) {
+        const circle = document.createElement('div')
+        circle.classList.add('circle')
+        
+        var width, height;
+
+        if (isSafari) {
+            width = Math.random() * (700 - 400) + 400
+            height = Math.random() * (700 - 400) + 400
+        } else {
+            width = Math.random() * (900 - 700) + 700
+            height = Math.random() * (900 - 700) + 700
+        }
+        const color = Math.floor(Math.random() * 16777215).toString(16)
+        // circle.style.backgroundColor = `#${color}`
+        circle.style.backgroundColor = 'transparent'
+
+        if (isSafari) {
+            circle.style.width = '0.1px'
+            circle.style.height = '0.1px'
+        } else {
+            circle.style.width = `${width}px`
+            circle.style.height = `${height}px`
+        }
+
+        if (isSafari) {
+            circle.style.boxShadow = `0 0 ${height}px ${width}px #${color}`
+        } else {
+            circle.style.boxShadow = `#${color} ${width}px ${height}px 15000px`
+            circle.style.opacity = 0.3
+        }
+
+        const randomXY = randomPosition(width, height);
+
+        circle.style.transform = `translate(${randomXY.x}px, ${randomXY.y}px)`
+        
+        const timeline = new TimelineMax({repeat: -1, yoyo: true})
+        timeline.to(circle, 10, {
+            bezier: {
+                type: 'soft',
+                curviness: 2,
+                values: [
+                    randomPosition(width, height),
+                    randomPosition(width, height)
+                ],
+            },
+            ease: Power1.easeInOut,
+            force3D: false,
+        })
+
+
+        bg.appendChild(circle)
+    }
+}
+
+window.onload = start()
